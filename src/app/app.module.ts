07b25503807e3a65c39493cef24ae7c6fe12a78d
FlorenceLegrand <flo.legrand@sfr.fr>
2018-04-03 14:35:41 +0200

    fonctionnement ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule }       from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
 
 import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- #1 import component
-import { HeroListComponent }   from './hero-list/hero-list.component';
 import { ListService } from './list.service';
+import { ListsComponent } from './lists/lists.component';
+import { ListViewComponent } from './list-view/list-view.component';
 
 
 
@@ -15,14 +15,14 @@ import { ListService } from './list.service';
     ReactiveFormsModule // <-- #2 add to @NgModule imports
   ],
   declarations: [
-    AppComponent,
-    HeroDetailComponent,
-    HeroListComponent
+    AppComponent,ListsComponent
+    ,ListViewComponent
+    
   ],
   exports: [ // export for the DemoModule
-    AppComponent,
-    HeroDetailComponent,
-    HeroListComponent
+    AppComponent,ListsComponent
+    ,ListViewComponent
+    
   ],
   providers: [ ListService ], // <-- #4 provide HeroService
   bootstrap: [ AppComponent ]
